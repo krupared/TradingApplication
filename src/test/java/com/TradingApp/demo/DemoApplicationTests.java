f464e028350ff6cac7e4f83e7f8bcee7964844af
krupared <kothakapukrupareddy@gmail.com>
2024-10-28 17:45:58 -0400

    added missed files

---
+++ src/test/java/com/TradingApp/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TradingApp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
